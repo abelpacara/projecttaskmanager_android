d53cef29bbb465ea2799c6a386d9f600018e7292
Abel Pacara <abel.pacara@gmail.com>
2017-10-30 14:48:28 -0400

    guarda foto de SOPORTE Y MANTENIMIENTO

--- app/src/main/java/com/boliviawebdesign/projecttaskmanager/AddComment.java
+++ app/src/main/java/com/boliviawebdesign/projecttaskmanager/AddComment.java
@@ -3,14 +3,10 @@ package com.boliviawebdesign.projecttaskmanager;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.util.Hashtable;
@@ -31,26 +27,11 @@ public class AddComment extends AppCompatActivity {
 
         btnAddComment.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-
-                String params[] = {commentContentText.getText().toString()
-                };
-
-
+                String params[] = {commentContentText.getText().toString()};
                 new ProcessJSON().execute(params);
             }
         });
-
-
     }
-
-
-
-
-
-
-
-
 
     private class ProcessJSON extends AsyncTask<String, Void, String> {
         protected String doInBackground(String ... params){
--- app/src/main/java/com/boliviawebdesign/projecttaskmanager/SenderReceiver.java
+++ app/src/main/java/com/boliviawebdesign/projecttaskmanager/SenderReceiver.java
@@ -14,21 +14,6 @@ import java.net.URLEncoder;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
-import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.Enumeration;
-import java.util.Hashtable;
 
 /**
  * Created by abel on 16-02-16.
@@ -36,6 +21,52 @@ import java.util.Hashtable;
 public class SenderReceiver {
     String stringStream;
 
+    // "http://192.168.134.204/orders/index.php/services/add_product"
+    public String getJSONfromURL(String stringURL, Hashtable params) {
+        try {
+            URL url = new URL(stringURL);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.setDoOutput(true);
+            urlConnection.setDoInput(true);
+
+            OutputStreamWriter writer = new OutputStreamWriter(urlConnection.getOutputStream());
+
+            writer.write( getWrappedToURL(params) );
+            writer.flush();
+
+            // Check the connection status
+            if(urlConnection.getResponseCode() == 200) {
+                // if response code = 200 ok
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                // Read the BufferedInputStream
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                stringStream = sb.toString();
+
+                reader.close();
+                urlConnection.disconnect();
+            }
+            else {
+                // Do something
+            }
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        finally{
+        }
+        return stringStream;
+    }
+    //////////////////////////////////////////////////////////////////////////////////////
     // "http://192.168.134.204/orders/index.php/services/add_product"
     public String getMessage(String stringURL, Hashtable params) {
         try {
